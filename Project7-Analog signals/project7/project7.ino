494b70db670fe38e64f661553a80be256981a121
gilimiz <gilimizrchi@gmail.com>
2026-07-13 14:28:38 +0300

    dim without pwm

--- Project7-Analog signals/project7/project7.ino
+++ Project7-Analog signals/project7/project7.ino
@@ -1,14 +1,31 @@
 const int rotaryPin = A0;
+const int ledPin = 3;
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
+  static int lastRotaryValue = -1;
+
   int rotaryValue = analogRead(rotaryPin);
-  Serial.println(rotaryValue);
-  delay(200);
+
+  if (rotaryValue != lastRotaryValue)
+  {
+    int pwmValue = map(rotaryValue, 0, 1023, 0, 255);
+    analogWrite(ledPin, pwmValue);
+
+    Serial.print("Rotary: ");
+    Serial.print(rotaryValue);
+    Serial.print(" -> PWM: ");
+    Serial.println(pwmValue);
+
+    lastRotaryValue = rotaryValue;
+  }
+
+  delay(50);
 }
 
