06db5bec408f9584d1c4d4fd8b823559c97e7a44
gilimiz <gilimizrchi@gmail.com>
2026-07-15 15:15:22 +0300

    oled+buzzer working!

---
+++ Project11-Final_Project_Part_1/Oled_Display/Oled_Display.ino
@@ -0,0 +1,75 @@
+#include "Arduino_SensorKit.h"
+#include <Servo.h>
+
+const int FAN_POWER_PIN = 7;
+const int SERVO_PIN = 3;
+const int BUZZER_PIN = 5;
+const int ANGLE_THRESHOLD = 45;
+const int SERVO_MIN_ANGLE = 0;
+const int SERVO_MAX_ANGLE = 170;
+const int FAN_ACTIVE_MAX_ANGLE = 45;
+
+Servo fanServo;
+int currentServoAngle = 90;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  Oled.begin();
+  Oled.setFlipMode(true);
+
+  pinMode(FAN_POWER_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  fanServo.attach(SERVO_PIN);
+  setServoAngle(90);
+  Accelerometer.begin();
+
+  noTone(BUZZER_PIN);
+}
+
+void loop() {
+  updateServoFromAccelerometer();
+  bool buzzerOn = (currentServoAngle <= FAN_ACTIVE_MAX_ANGLE);
+
+  if (buzzerOn) {
+    tone(BUZZER_PIN, 2000);
+  } else {
+    noTone(BUZZER_PIN);
+  }
+
+  displayStatus(currentServoAngle, buzzerOn);
+  delay(200);
+}
+
+void setFanPower(bool enabled) {
+  digitalWrite(FAN_POWER_PIN, enabled ? HIGH : LOW);
+}
+
+void setServoAngle(int angle) {
+  currentServoAngle = constrain(angle, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
+  fanServo.write(currentServoAngle);
+  setFanPower(currentServoAngle <= FAN_ACTIVE_MAX_ANGLE);
+}
+
+void updateServoFromAccelerometer() {
+  float xValue = Accelerometer.readX();
+  float ratio = constrain(xValue, -1.0f, 1.0f);
+  int mappedAngle = (int)((ratio + 1.0f) * 85.0f);
+  setServoAngle(mappedAngle);
+}
+
+void displayStatus(int angle, bool buzzerOn) {
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+
+  Oled.setCursor(0, 0);
+  Oled.print("Angle: ");
+  Oled.print(angle);
+
+  Oled.setCursor(0, 3);
+  Oled.print("Buzzer: ");
+  Oled.print(buzzerOn ? "ON" : "OFF");
+}
